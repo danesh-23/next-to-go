abf3ee1befed4715a8015b56f97f066fbced42f1
Danesh Rajasolan <daneshpbc@gmail.com>
2025-04-14 13:11:26 +1000

    Add app icon and color scheme to match Neds

---
+++ NextToGo/Presentation/UI Components/Color+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extensions.swift
+//  NextToGo
+//
+//  Created by Danesh Rajasolan on 2025-04-14.
+//
+
+import SwiftUI
+
+extension Color {
+    static let nedsOrange = Color(red: 1.0, green: 0.4, blue: 0.0) // Approximate to Neds branding
+}
